b4d9e39903c208adfa867399feee07af655bdfef
Amir Zuhdi Wibowo <94032140+cakadi190@users.noreply.github.com>
2024-11-21 14:35:16 +0700

    feat(home): add featured and overview sections

--- inertia/pages/web/home.tsx
+++ inertia/pages/web/home.tsx
@@ -1,14 +1,18 @@
-import { Head } from '@inertiajs/react';
 import React from 'react';
-import Header from '~/components/contents/home/header';
+import HeaderSection from '~/components/contents/home/header';
+import OverviewSection from '~/components/contents/home/overview';
+import { Head } from '@inertiajs/react';
 import { SeoType } from '~/types/seo-types';
+import FeaturedSection from '~/components/contents/home/featured';
 
 export default function Home({ title }: React.PropsWithChildren<SeoType>) {
   return (
     <>
       <Head title={title} />
 
-      <Header />
+      <HeaderSection />
+      <FeaturedSection />
+      <OverviewSection />
     </>
   );
 };
