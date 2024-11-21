2481ae7f489bb99253d3b21c981593e5355d402c
Amir Zuhdi Wibowo <94032140+cakadi190@users.noreply.github.com>
2024-11-21 14:45:24 +0700

    chore(deps): add image optimization plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite'
-import { getDirname } from '@adonisjs/core/helpers'
-import inertia from '@adonisjs/inertia/client'
-import react from '@vitejs/plugin-react'
-import adonisjs from '@adonisjs/vite/client'
+import { defineConfig } from 'vite';
+import { getDirname } from '@adonisjs/core/helpers';
+import inertia from '@adonisjs/inertia/client';
+import react from '@vitejs/plugin-react';
+import adonisjs from '@adonisjs/vite/client';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    react(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    ViteImageOptimizer({
+      jpeg: {
+        quality: 80,
+      },
+      png: {
+        quality: 80,
+      },
+      webp: {
+        quality: 75,
+      },
+      avif: {
+        quality: 50,
+      },
+      svg: {
+        multipass: true,
+      },
+    }),
+  ],
 
   /**
    * Define aliases for importing modules from
